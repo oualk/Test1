506e570600a0038ada880632d5405e2e8046db5d
Oualid Ben Khecib <>
2024-12-13 17:51:19 +0100

    added shared cpp file

--- proj.android/app/src/main/cpp/native-lib.cpp
+++ proj.android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,10 @@
 #include <jni.h>
 #include <string>
-
+#include "../../../../../shared_cpp/test.h"
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_test1_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = testfunction();
     return env->NewStringUTF(hello.c_str());
 }
---
+++ shared_cpp/test.cpp
@@ -0,0 +1,8 @@
+
+#include <string>
+
+std::string testfunction()
+{
+  std::string word = "test function from shared";
+  return word;
+}
